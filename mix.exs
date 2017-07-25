8b70807464993deecf47c9b147e4837b17456f21
Travis Poulsen <travis.poulsen@gmail.com>
2017-07-24 22:45:18 -0400

    Update readme

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule RecursionSchemes.Mixfile do
 
   def project do
     [app: :recursion_schemes,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
